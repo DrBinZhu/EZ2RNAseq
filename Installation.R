23a16ef92849e747f7b68beb081c6c112f97ef02
DrBinZhu <54455586+DrBinZhu@users.noreply.github.com>
2022-05-12 10:23:05 -0400

    Delete Installation.R

--- Installation.R
+++
@@ -1,5 +0,0 @@
-###################################################################
-# The silly version of DESeq2 script is edited by Dr. Bin Zhu at Virginia Commonwealth University
-# Email: binzhu0824@gmail.com
-#Package installation, run only once on your computer
-BiocManager::install(c("DESeq", "DESeq2","limma", "pasilla","pheatmap","vsn","RColorBrewer","ggplot2","hexbin","ggrepel"))
