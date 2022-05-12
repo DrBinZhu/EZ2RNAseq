d4c1c853c3ee204ba8dccd3abc03547815ccf3cc
DrBinZhu <54455586+DrBinZhu@users.noreply.github.com>
2022-05-12 10:22:51 -0400

    Update EZ2RNAseq.R

--- EZ2RNAseq.R
+++ EZ2RNAseq.R
@@ -1,10 +1,40 @@
-###################################################################
-# This version of DESeq2 script is edited by Dr. Bin Zhu at Virginia Commonwealth University
-# Email: binzhu0824@gmail.com
-###################################################################
-###################################################################
-##Start##
+##### install packages #####
+ip <- as.data.frame(installed.packages())
+ip <- ip$Package
 
+if (sum(ip == "rstudioapi") == 0) {
+  install.packages("rstudioapi")
+}
+
+if (sum(ip == "vsn") == 0) {
+  install.packages("vsn")
+}
+
+if (sum(ip == "DESeq2") == 0) {
+  install.packages("DESeq2")
+}
+
+if (sum(ip == "hexbin") == 0) {
+  install.packages("hexbin")
+}
+
+if (sum(ip == "pheatmap") == 0) {
+  install.packages("pheatmap")
+}
+
+if (sum(ip == "RColorBrewer") == 0) {
+  install.packages("RColorBrewer")
+}
+
+if (sum(ip == "ggplot2") == 0) {
+  install.packages("ggplot2")
+}
+
+if (sum(ip == "ggrepel") == 0) {
+  install.packages("ggrepel")
+}
+
+###################################################################
 #load DESeq package;
 library("DESeq2")
 library("vsn")
@@ -20,7 +50,10 @@ setwd(dirname(rstudioapi::getSourceEditorContext()$path))
 cts= read.csv("data.csv", header=TRUE, row.names=1)
 design = read.csv("design.csv", header=TRUE, row.names=1)
 
-
+path_input = dirname(rstudioapi::getSourceEditorContext()$path)
+path_output = paste0(path_input,'/results')
+dir.create(path_output)
+setwd(path_output)
 
 factor_number <- ncol(design)
 
@@ -52,19 +85,18 @@ if (factor_number == 1) {
   
   n=length(res)
   
-  lable_list = as.matrix(read.csv('lable.csv', header = F))
-  for (i in 2:n)   {
+  i=2
     res2 <- results(dds, name=res[i])
     write.csv(res2,file = paste(res1[i],".csv"))
     
     # volcano plot
-    lable_list = file.exists('lable.csv')
+    lable_list = file.exists(paste0(path_input,'/lable.csv'))
     if (lable_list) {
-      lable_list = file.info('lable.csv')
+      lable_list = file.info(paste0(path_input,'/lable.csv'))
       lable_list = lable_list$size
       
       if (lable_list != 0) {
-        lable_list = as.matrix(read.csv('lable.csv', header = F))
+        lable_list = as.matrix(read.csv(paste0(path_input,'/lable.csv'), header = F))
       } else {
         lable_list = NA
       }
@@ -73,45 +105,45 @@ if (factor_number == 1) {
       lable_list = NA
     }
     
-    if (i != 4) {
-      volvano_plot <- as.data.frame(res2@listData)
+    
+      volcano_plot <- as.data.frame(res2@listData)
       
-      volvano_plot$gene_id = row.names(cts)
-      volvano_plot$lable_name = NA
-      volvano_plot$lable_name[volvano_plot$gene_id %in% lable_list] <- volvano_plot$gene_id[volvano_plot$gene_id %in% lable_list]
+      volcano_plot$gene_id = row.names(cts)
+      volcano_plot$lable_name = NA
+      volcano_plot$lable_name[volcano_plot$gene_id %in% lable_list] <- volcano_plot$gene_id[volcano_plot$gene_id %in% lable_list]
       
-      volvano_plot$color <- 'grey'
-      volvano_plot$color[volvano_plot$log2FoldChange >= 1 & volvano_plot$padj <= 0.05] = 'green'
-      volvano_plot$color[volvano_plot$log2FoldChange <= -1 & volvano_plot$padj <= 0.05] = 'red'
+      volcano_plot$color <- 'grey'
+      volcano_plot$color[volcano_plot$log2FoldChange >= 1 & volcano_plot$padj <= 0.05] = 'green'
+      volcano_plot$color[volcano_plot$log2FoldChange <= -1 & volcano_plot$padj <= 0.05] = 'red'
       
       
-      volvano_plot$padj <- -log10(volvano_plot$padj)
+      volcano_plot$padj <- -log10(volcano_plot$padj)
       
-      p=ggplot(data=volvano_plot, aes(x=log2FoldChange, y=padj, color = color, label=lable_name)) + 
-        geom_point() + 
-        scale_color_manual(values=c("green", "grey", "red")) +
+      ggplot(data=volcano_plot, aes(x=log2FoldChange, y=padj, label=lable_name)) + 
+        geom_point(aes(color = color)) + 
+        scale_color_manual(values=c("green" = "green", "grey" = "grey", "red" = "red")) +
         theme(legend.position = "none") +
         geom_text_repel(aes(label = lable_name),size = 3, colour = 'black') 
       
-      print(p)
+      ggsave(paste0(col_name[i-1],"_volcano.pdf"), width=4, height=4)
       
-      x_limit_neg <- min(quantile(volvano_plot$log2FoldChange,.01, na.rm =T),-2)
-      x_limit_pos <- max(quantile(volvano_plot$log2FoldChange,.99, na.rm =T),2)
-      volvano_plot$log2FoldChange[volvano_plot$log2FoldChange < x_limit_neg] = x_limit_neg
-      volvano_plot$log2FoldChange[volvano_plot$log2FoldChange > x_limit_pos] = x_limit_pos
+      x_limit_neg <- min(quantile(volcano_plot$log2FoldChange,.01, na.rm =T),-2)
+      x_limit_pos <- max(quantile(volcano_plot$log2FoldChange,.99, na.rm =T),2)
+      volcano_plot$log2FoldChange[volcano_plot$log2FoldChange < x_limit_neg] = x_limit_neg
+      volcano_plot$log2FoldChange[volcano_plot$log2FoldChange > x_limit_pos] = x_limit_pos
       
-      y_limit_pos <- max(quantile(volvano_plot$padj,.99, na.rm =T),2)
-      volvano_plot$padj[volvano_plot$padj > y_limit_pos] = y_limit_pos
+      y_limit_pos <- max(quantile(volcano_plot$padj,.99, na.rm =T),2)
+      volcano_plot$padj[volcano_plot$padj > y_limit_pos] = y_limit_pos
       
-      p=ggplot(data=volvano_plot, aes(x=log2FoldChange, y=padj, color = color, label=lable_name)) + 
-        geom_point() + 
-        scale_color_manual(values=c("green", "grey", "red")) +
+      ggplot(data=volcano_plot, aes(x=log2FoldChange, y=padj, label=lable_name)) + 
+        geom_point(aes(color = color)) + 
+        scale_color_manual(values=c("green" = "green", "grey" = "grey", "red" = "red")) +
         theme(legend.position = "none") +
         geom_text_repel(aes(label = lable_name),size = 3, colour = 'black') 
       
-      print(p)
-    }
-  }
+      ggsave(paste0(col_name[i-1],"_volcano_2.pdf"), width=4, height=4)
+
+
   
 
   
@@ -127,17 +159,21 @@ if (factor_number == 1) {
                   decreasing=TRUE)[1:20]
   df <- as.data.frame(colData(dds)[1])
   colnames(df) <- c(factor_1)
-  pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
-           cluster_cols=TRUE, annotation_col=df)
+  pdf("pheatmap_1.pdf", width=6, height=6)
+  print(pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+                 cluster_cols=TRUE, annotation_col=df))
+  
+  dev.off()
   
   select <- order(rowMeans(counts(dds,normalized=TRUE)),
                   decreasing=TRUE)[1:nrow(cts)]
   df <- as.data.frame(colData(dds)[1])
   colnames(df) <- c(factor_1)
-  pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
-           cluster_cols=TRUE, annotation_col=df)
   
-  
+  pdf("pheatmap_2.pdf", width=6, height=6)
+  print(pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+                 cluster_cols=TRUE, annotation_col=df))
+  dev.off()  
   
   ############### Heatmap of the sample-to-sample distances ###############
   sampleDists <- dist(t(assay(vsd)))
@@ -146,10 +182,13 @@ if (factor_number == 1) {
   rownames(sampleDistMatrix) <- paste(vsd$factor_1)     
   colnames(sampleDistMatrix) <- NULL
   colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-  pheatmap(sampleDistMatrix,
-           clustering_distance_rows=sampleDists,
-           clustering_distance_cols=sampleDists,
-           col=colors)
+
+  pdf("pheatmap_3.pdf", width=6, height=6)
+  print(pheatmap(sampleDistMatrix,
+                 clustering_distance_rows=sampleDists,
+                 clustering_distance_cols=sampleDists,
+                 col=colors))
+  dev.off()
   
   
   ############### pincipal component plot of the samples ###############
@@ -161,10 +200,19 @@ if (factor_number == 1) {
   
   ggplot(pcaData, aes(PC1, PC2, color=factor_1)) +
     geom_point(size=3) +
-    labs(color=factor_1) +
+    labs(shape=factor_1, color=factor_2) +
     xlab(paste0("PC1: ",percentVar[1],"% variance")) +
     ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
     coord_fixed()
+  ggsave(("PCA.pdf"), width=4, height=4)
+  
+  ggplot(pcaData, aes(PC1, PC2, color=factor_1)) +
+    geom_point(size=3) +
+    labs(shape=factor_1, color=factor_2) +
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+    coord_fixed()+stat_ellipse(type = "t")
+  ggsave(("PCA_2.pdf"), width=4, height=4)
   
   
   
@@ -200,20 +248,21 @@ if (factor_number == 1) {
   res1
   
   n=length(res)
+  res2 <- results(dds, name=res[4])
+  write.csv(res2,file = paste(res1[4],".csv"))
   
-  lable_list = as.matrix(read.csv('lable.csv', header = F))
-  for (i in 2:n)   {
+  for (i in 2:3)   {
     res2 <- results(dds, name=res[i])
     write.csv(res2,file = paste(res1[i],".csv"))
     
     # volcano plot
-    lable_list = file.exists('lable.csv')
+    lable_list = file.exists(paste0(path_input,'/lable.csv'))
     if (lable_list) {
-      lable_list = file.info('lable.csv')
+      lable_list = file.info(paste0(path_input,'/lable.csv'))
       lable_list = lable_list$size
       
       if (lable_list != 0) {
-        lable_list = as.matrix(read.csv('lable.csv', header = F))
+        lable_list = as.matrix(read.csv(paste0(path_input,'/lable.csv'), header = F))
       } else {
         lable_list = NA
       }
@@ -222,45 +271,45 @@ if (factor_number == 1) {
       lable_list = NA
     }
     
-    if (i != 4) {
-      volvano_plot <- as.data.frame(res2@listData)
+
+      volcano_plot <- as.data.frame(res2@listData)
       
-      volvano_plot$gene_id = row.names(cts)
-      volvano_plot$lable_name = NA
-      volvano_plot$lable_name[volvano_plot$gene_id %in% lable_list] <- volvano_plot$gene_id[volvano_plot$gene_id %in% lable_list]
+      volcano_plot$gene_id = row.names(cts)
+      volcano_plot$lable_name = NA
+      volcano_plot$lable_name[volcano_plot$gene_id %in% lable_list] <- volcano_plot$gene_id[volcano_plot$gene_id %in% lable_list]
       
-      volvano_plot$color <- 'grey'
-      volvano_plot$color[volvano_plot$log2FoldChange >= 1 & volvano_plot$padj <= 0.05] = 'green'
-      volvano_plot$color[volvano_plot$log2FoldChange <= -1 & volvano_plot$padj <= 0.05] = 'red'
+      volcano_plot$color <- 'grey'
+      volcano_plot$color[volcano_plot$log2FoldChange >= 1 & volcano_plot$padj <= 0.05] = 'green'
+      volcano_plot$color[volcano_plot$log2FoldChange <= -1 & volcano_plot$padj <= 0.05] = 'red'
       
       
-      volvano_plot$padj <- -log10(volvano_plot$padj)
+      volcano_plot$padj <- -log10(volcano_plot$padj)
       
-      p=ggplot(data=volvano_plot, aes(x=log2FoldChange, y=padj, color = color, label=lable_name)) + 
-        geom_point() + 
-        scale_color_manual(values=c("green", "grey", "red")) +
+      ggplot(data=volcano_plot, aes(x=log2FoldChange, y=padj, label=lable_name)) + 
+        geom_point(aes(color = color)) + 
+        scale_color_manual(values=c("green" = "green", "grey" = "grey", "red" = "red")) +
         theme(legend.position = "none") +
         geom_text_repel(aes(label = lable_name),size = 3, colour = 'black') 
       
-      print(p)
+      ggsave(paste0(col_name[i-1],"_volcano.pdf"), width=4, height=4)
       
-      x_limit_neg <- min(quantile(volvano_plot$log2FoldChange,.01, na.rm =T),-2)
-      x_limit_pos <- max(quantile(volvano_plot$log2FoldChange,.99, na.rm =T),2)
-      volvano_plot$log2FoldChange[volvano_plot$log2FoldChange < x_limit_neg] = x_limit_neg
-      volvano_plot$log2FoldChange[volvano_plot$log2FoldChange > x_limit_pos] = x_limit_pos
+      x_limit_neg <- min(quantile(volcano_plot$log2FoldChange,.01, na.rm =T),-2)
+      x_limit_pos <- max(quantile(volcano_plot$log2FoldChange,.99, na.rm =T),2)
+      volcano_plot$log2FoldChange[volcano_plot$log2FoldChange < x_limit_neg] = x_limit_neg
+      volcano_plot$log2FoldChange[volcano_plot$log2FoldChange > x_limit_pos] = x_limit_pos
       
-      y_limit_pos <- max(quantile(volvano_plot$padj,.99, na.rm =T),2)
-      volvano_plot$padj[volvano_plot$padj > y_limit_pos] = y_limit_pos
+      y_limit_pos <- max(quantile(volcano_plot$padj,.99, na.rm =T),2)
+      volcano_plot$padj[volcano_plot$padj > y_limit_pos] = y_limit_pos
       
-      p=ggplot(data=volvano_plot, aes(x=log2FoldChange, y=padj, color = color, label=lable_name)) + 
-        geom_point() + 
-        scale_color_manual(values=c("green", "grey", "red")) +
+      ggplot(data=volcano_plot, aes(x=log2FoldChange, y=padj, label=lable_name)) + 
+        geom_point(aes(color = color)) + 
+        scale_color_manual(values=c("green" = "green", "grey" = "grey", "red" = "red")) +
         theme(legend.position = "none") +
         geom_text_repel(aes(label = lable_name),size = 3, colour = 'black') 
       
-      print(p)
+      ggsave(paste0(col_name[i-1],"_volcano_2.pdf"), width=4, height=4)
     }
-  }
+  
 
   vsd <- varianceStabilizingTransformation(dds)  # transfer data using a variance stabilizing transformation
 
@@ -270,16 +319,22 @@ if (factor_number == 1) {
                   decreasing=TRUE)[1:20]
   df <- as.data.frame(colData(dds)[,c("factor_1","factor_2")])
   colnames(df) <- c(factor_1,factor_2)
-  pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
-           cluster_cols=TRUE, annotation_col=df)
+  
+  pdf("pheatmap_1.pdf", width=6, height=6)
+  print(pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+                   cluster_cols=TRUE, annotation_col=df))
+
+  dev.off()
   
   select <- order(rowMeans(counts(dds,normalized=TRUE)),
                   decreasing=TRUE)[1:nrow(cts)]
   df <- as.data.frame(colData(dds)[,c("factor_1","factor_2")])
   colnames(df) <- c(factor_1,factor_2)
-  pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
-           cluster_cols=TRUE, annotation_col=df)
   
+  pdf("pheatmap_2.pdf", width=6, height=6)
+  print(pheatmap(assay(ntd)[select,], cluster_rows=TRUE, show_rownames=TRUE,
+                 cluster_cols=TRUE, annotation_col=df))
+  dev.off()
   
   sampleDists <- dist(t(assay(vsd)))
   
@@ -287,11 +342,13 @@ if (factor_number == 1) {
   rownames(sampleDistMatrix) <- paste(vsd$factor_1, vsd$factor_2, sep="_")     
   colnames(sampleDistMatrix) <- NULL
   colors <- colorRampPalette( rev(brewer.pal(9, "Blues")) )(255)
-  pheatmap(sampleDistMatrix,
-           clustering_distance_rows=sampleDists,
-           clustering_distance_cols=sampleDists,
-           col=colors)
   
+  pdf("pheatmap_3.pdf", width=6, height=6)
+  print(pheatmap(sampleDistMatrix,
+                 clustering_distance_rows=sampleDists,
+                 clustering_distance_cols=sampleDists,
+                 col=colors))
+  dev.off()
 
   pcaData <- plotPCA(vsd, intgroup=c("factor_2", "factor_1"), returnData=TRUE)
   percentVar <- round(100 * attr(pcaData, "percentVar"))
@@ -304,7 +361,15 @@ if (factor_number == 1) {
     xlab(paste0("PC1: ",percentVar[1],"% variance")) +
     ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
     coord_fixed()
+  ggsave(("PCA.pdf"), width=4, height=4)
   
+  ggplot(pcaData, aes(PC1, PC2, color=factor_2, shape=factor_1)) +
+    geom_point(size=3) +
+    labs(shape=factor_1, color=factor_2) +
+    xlab(paste0("PC1: ",percentVar[1],"% variance")) +
+    ylab(paste0("PC2: ",percentVar[2],"% variance")) + 
+    coord_fixed()+stat_ellipse(type = "t")
+  ggsave(("PCA_2.pdf"), width=4, height=4)
   
 } else {
   print("Only one or two factors are avaiable in the script")
